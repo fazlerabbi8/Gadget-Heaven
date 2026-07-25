04b5a114447d4ff3080b146cbed8e7c28825487c
fazlerabbi13 <mohammad0132794@gmail.com>
2026-07-26 02:10:44 +0600

    loaded data in all card.

--- src/components/Products/ProductCard.jsx
+++ src/components/Products/ProductCard.jsx
@@ -2,8 +2,8 @@ const ProductCard = ({ product }) => {
   const {product_image, price,product_title } = product;
   return (
     <div className="card bg-base-100 w-96 shadow-sm">
-      <figure>
-        <img
+      <figure className="h-56 overflow-hidden">
+        <img className=""className="w-full h-full object-cover"
           src={product_image}
           alt="Shoes"
         />
